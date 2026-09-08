fa5e8c1c0f5c48fe3d04097339facd8b37e75af8
Progress4002 <progress.akhelumele@outlook.com>
2026-09-09 01:09:07 +0200

    feat: implement factroial methode

--- pom.xml/src/main/java/at/campus02/bsd/Calculator.java
+++ pom.xml/src/main/java/at/campus02/bsd/Calculator.java
@@ -20,6 +20,14 @@ public class Calculator {
         return a / b;
     }
     public int factorial(int number) {
-        return 0;
+        if (number < 0) {
+            return 0;
+        }
+
+        int result = 1;
+        for (int i = 1; i <= number; i++) {
+            result *= i;
+        }
+        return result;
     }
 }
